75e2b1aff273019c4fbbeb9096ce9e35d89978cd
11vanshika <vanshikaa@moback.com>
2022-10-03 20:01:18 +0530

    first commit

---
+++ Interface/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Program
+    {
+         interface ITestInterface1
+        {
+            void Add(int x, int y);
+        }
+        interface ITestInterface2
+        {
+            void Sub(int x, int y);
+        }
+        class ImplementationClass : ITestInterface2
+        {
+            public void Add(int x,int y ) {
+                Console.WriteLine(x + y);
+            }
+            public void Sub(int x, int y) {
+                Console.WriteLine(x - y);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ImplementationClass obj = new ImplementationClass();
+            obj.Add(1,2);
+            obj.Sub(2,1);
+        }
+    }
+}
---
+++ abstractClass/abstractClass/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractClass
+{
+    internal class Program
+    { 
+        abstract class Abparent
+        {
+            public void Add(int x , int y)
+            {
+                Console.WriteLine(x + y);
+            }
+            public void Sub(int x , int y)
+            {
+                Console.WriteLine(x - y);
+            }
+            public abstract void Mul(int x, int y);
+            public abstract void Divide(int x, int y);
+        }
+        class Abchild : Abparent
+        {
+            public override void Mul(int x, int y)
+            {
+                Console .WriteLine(x * y);
+            }
+            public override void Divide(int x, int y)
+            {
+                Console.WriteLine(x / y);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Create object from child class
+            Abchild childClassObj = new Abchild();
+            Abparent parentClassObj = childClassObj;
+
+            parentClassObj.Add(23, 34);
+            parentClassObj.Sub(45 ,32);
+            childClassObj.Mul(25, 6);
+            childClassObj.Divide(25, 5);
+        }
+    }
+}
